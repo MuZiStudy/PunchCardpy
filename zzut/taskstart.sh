f9e9dae95ec894b0eb17a49c2a29be4da4391e6e
WindSnowLi <61129607+WindSnowLi@users.noreply.github.com>
2020-11-13 18:49:21 +0800

    优化启动文件

--- zzut/taskstart.sh
+++ zzut/taskstart.sh
@@ -1,2 +1,2 @@
 ps -aux|grep "zzutPunchCard"|grep -v grep|awk '{print "kill -9 "$2}'|sh
-nohup python3 ./zzutPunchCard.py >/dev/null 2>/dev/null &
+nohup python3 $(pwd)/zzutPunchCard.py >/dev/null 2>/dev/null &
